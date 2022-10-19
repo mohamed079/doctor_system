0f7d81fa0d8b5f1a100dcfdf29ae6b60ba233549
Mohamed Bassem <49068234+mohamed079@users.noreply.github.com>
2022-10-19 08:47:33 +0200

    doctor system

--- reservation/models.py
+++ reservation/models.py
@@ -11,9 +11,7 @@ class Reservation (models.Model):
 
     patient = models.ForeignKey(Patient , related_name="reservations" , on_delete=models.CASCADE)
     doctor = models.ForeignKey(Doctor , related_name="reservations" , on_delete=models.CASCADE)
-    visit_date = models.DateTimeField()
     description = models.TextField(default="" , null=True , blank= True)
-    is_paid = models.BooleanField()
     status = models.CharField(choices=Status.choices , default=Status.PENDING , max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
