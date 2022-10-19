cda6d99e5ee5a7cee97761f783c96a73b4c3b6a1
Mohamed Bassem <49068234+mohamed079@users.noreply.github.com>
2022-10-19 05:06:19 +0200

    update doctor

--- doctor/models.py
+++ doctor/models.py
@@ -8,7 +8,6 @@ class Doctor (ExtendUser):
     degree = models.CharField(max_length=256)
     department = models.ForeignKey(Department , related_name="doctors" , on_delete=models.CASCADE)
     fees = models.DecimalField(max_digits=4 , decimal_places=0)
-    balance_amount = models.DecimalField(max_digits=6 , decimal_places=0 , default=0 , null = True , blank = True)
 
 
     
